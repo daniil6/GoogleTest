5532d8ce7e0032256016b0030bbe73c95d48d61b
Daniil <daniil.arduino@yandex.ru>
2021-07-18 10:47:42 +0300

    Fix BracketsStack: add cycle, add BracketsBool, add BracketsCount

--- brackets/brackets_test.h
+++ brackets/brackets_test.h
@@ -1,7 +1,8 @@
 #ifndef GTBRACKETS_H
 #define GTBRACKETS_H
 
-#include "include/check/brackets.h"
+#include "check/brackets.h"
+#include "wparameters.h"
 
 class GTBrackets : public ::testing::Test
 {
@@ -10,45 +11,190 @@ protected:
     void TearDown() override{};
 };
 
-TEST_F(GTBrackets, CheckBrackets)
+TEST_F(GTBrackets, BracketsStack)
 {
     bool result = false;
+    uint8_t count = 0;
 
-    result = Brackets(")()");
-    ASSERT_EQ(result, false);
+    int i = 0;
+    while(i++ != COUNT_TEST) {
 
-    result = Brackets("))()");
-    ASSERT_EQ(result, false);
+        result = BracketsStack(")()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
 
-    result = Brackets("()()");
-    ASSERT_EQ(result, true);
+        result = BracketsStack("))()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
 
-    result = Brackets("())");
-    ASSERT_EQ(result, false);
+        result = BracketsStack("()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
 
-    result = Brackets("()(");
-    ASSERT_EQ(result, false);
+        result = BracketsStack("())", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
 
-    result = Brackets("(())");
-    ASSERT_EQ(result, true);
+        result = BracketsStack("()(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
 
-    result = Brackets("()()()()()()()");
-    ASSERT_EQ(result, true);
+        result = BracketsStack("(())", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
 
-    result = Brackets("(((((())))))(((((())))))()()()()()");
-    ASSERT_EQ(result, true);
+        result = BracketsStack("()()()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 14);
 
-    result = Brackets(")(");
-    ASSERT_EQ(result, false);
+        result = BracketsStack("(((((())))))(((((())))))()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 34);
 
-    result = Brackets("(((");
-    ASSERT_EQ(result, false);
+        result = BracketsStack(")(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
 
-    result = Brackets(")))(");
-    ASSERT_EQ(result, false);
+        result = BracketsStack("(((", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
 
-    result = Brackets("(()()()");
-    ASSERT_EQ(result, false);
+        result = BracketsStack(")))(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsStack("(()()()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 7);
+
+        result = BracketsStack("())(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+    }
+}
+
+TEST_F(GTBrackets, BracketsBool)
+{
+    bool result = false;
+    int count = 0;
+
+    int i = 0;
+    while(i++ != COUNT_TEST) {
+
+        result = BracketsBool(")()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsBool("))()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsBool("()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
+
+        result = BracketsBool("())", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsBool("()(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsBool("(())", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
+
+        result = BracketsBool("()()()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 14);
+
+        result = BracketsBool("(((((())))))(((((())))))()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 34);
+
+        result = BracketsBool(")(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsBool("(((", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsBool(")))(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsBool("(()()()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 7);
+
+        result = BracketsBool("())(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+    }
+}
+
+TEST_F(GTBrackets, BracketsCount)
+{
+    bool result = false;
+    int count = 0;
+
+    int i = 0;
+    while(i++ != COUNT_TEST) {
+
+        result = BracketsCount(")()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsCount("))()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsCount("()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
+
+        result = BracketsCount("())", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsCount("()(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsCount("(())", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 4);
+
+        result = BracketsCount("()()()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 14);
+
+        result = BracketsCount("(((((())))))(((((())))))()()()()()", count);
+        ASSERT_EQ(result, true);
+        ASSERT_EQ(count, 34);
+
+        result = BracketsCount(")(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsCount("(((", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+
+        result = BracketsCount(")))(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 1);
+
+        result = BracketsCount("(()()()", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 7);
+
+        result = BracketsCount("())(", count);
+        ASSERT_EQ(result, false);
+        ASSERT_EQ(count, 3);
+    }
 }
 
 #endif // GTBRACKETS_H
